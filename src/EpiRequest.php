27e4c07e84274693f91e3449809971a008190b70
Bartek Zdanowski <bzd@touk.pl>
2018-12-29 20:46:39 +0100

    initializing object from json request

--- src/EpiRequest.php
+++ src/EpiRequest.php
@@ -1,6 +1,8 @@
 <?php
 
 class EpiRequest {
+
+    const APP_JSON = 'application/json';
     private static $instance;
 
     public static function getInstance() {
@@ -12,10 +14,39 @@ class EpiRequest {
         return self::$instance;
     }
 
-    public function from($array, $class) {
-        $obj = new $class();
+    public function get($class) {
+        if ($this->contentType() == self::APP_JSON) {
+            $data = json_decode(file_get_contents('php://input'), true);
+        } else {
+            throw new Exception("Not supported type " . $this->contentType());
+        }
+
+        $obj = $this->initFrom($data, $class);
 
         return $obj;
+    }
+
+    private function initFrom($data, $class) {
+        $log = getLogger(__CLASS__);
+        $refl = new ReflectionClass($class);
+        if (!$refl->isInstantiable()) {
+            throw new Exception($class . ' is not instantable');
+        }
+
+        $args = [];
+        $constr = $refl->getConstructor();
+        if ($constr) {
+            foreach ($constr->getParameters() as $p) {
+                $args[] = isset($data[$p->getName()]) ? $data[$p->getName()] : null;
+            }
+        }
+
+        $log->debug("initializing " . $class . ' with args ' . var_export($args, true));
+        $obj = $refl->newInstanceArgs($args);
+    }
+
+    public function contentType() {
+        return isset($_SERVER["CONTENT_TYPE"]) ? $_SERVER["CONTENT_TYPE"] : null;
     }
 
 }
